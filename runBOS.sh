3b2f74917904168d050df68e6e44eef760b0f908
NickGraham <nickgraham@me.com>
2024-09-26 05:03:27 -0400

    fixed exit

--- runBOS.sh
+++ runBOS.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Navigate to the directory where your app is stored
-cd /home/deck/BOSctrl/base
+# Navigate to the directory where the Python script is located
+cd /home/deck/BOSctrl/base || exit
 
 # Activate the virtual environment
 source /home/deck/BOSctrl/bin/activate
 
 # Run the Python script
 python BOSmain.py
+
 
 # Keep the terminal open after the script finishes (for debugging or other tasks)
 exec bash
